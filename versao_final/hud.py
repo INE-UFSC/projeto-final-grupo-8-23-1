517a6ddaa904964456376a73e3b6084225776ee6
Pedro Henrique de Sena Trombini Taglialenha <phtaglialenha@gmail.com>
2023-06-12 11:50:42 -0300

    HUD implementado

--- versao_final/hud.py
+++ versao_final/hud.py
@@ -6,35 +6,51 @@ class Hud:
         self.controlador = controlador
         self.font = pygame.font.SysFont('bahnschrift', 35)
 
-        self.vida_icon = pygame.image.load('assets/vida.png')
-        self.troca_mundo_icon = pygame.image.load('assets/troca_de_mundo_texto.png')
+        self.vida_icon = pygame.image.load('assets/vida.png') # icone de vida
+        self.vidas_text = pygame.image.load('assets/vidas_texto.png') # texto "Vidas:"
+
+        self.troca_mundo_texto = pygame.image.load('assets/troca_de_mundo_texto.png') # texto "troca de mundo:"
+
         self.tempo_texto_icon = pygame.image.load('assets/tempo_texto.png')
-       # self.score_texto_icon = pygame.image.load('assets/tempo_texto.png')
+        self.tempo_icon = pygame.image.load('assets/relogio1.5.png')
+
+        self.score_texto = pygame.image.load('assets/score_texto.png')
+
+
         self.barra_vertical_icon = pygame.image.load('assets/linha_vertical.png')
+        self.barra_horizontal_icon = pygame.image.load('assets/linha_horizontal.png')
+
 
     def draw(self, screen):
+        screen.blit(self.barra_horizontal_icon, (0, 62))
+
         # Desenhar o número de vidas
+        screen.blit(self.vidas_text, (10, 20))
         for i in range(self.controlador.player.get_vidas()):
-            screen.blit(self.vida_icon, (10 + i * (self.vida_icon.get_width() + 10), 10))
-        vidas_text = self.font.render(str(self.controlador.vidas), True, (255, 255, 255))
+            screen.blit(self.vida_icon, (120 + i * (self.vida_icon.get_width() + 10), 10))
+        screen.blit(self.barra_vertical_icon, (280, 0))
+
 
         # Desenhar o tempo restante para a troca de mundo
-        screen.blit(self.troca_mundo_icon, (300, 20))
-        tempo_troca_mundo = max(0, (self.controlador.tempo_troca_de_fase - self.controlador.tempo_na_fase) // 1000) +1
+        screen.blit(self.tempo_icon, (300, 10))
+        screen.blit(self.troca_mundo_texto, (350, 20))
+        tempo_troca_mundo = max(0, (self.controlador.tempo_troca_de_fase - self.controlador.tempo_na_fase) // 1000) + 1
         tempo_troca_text = self.font.render(str(tempo_troca_mundo), True, (255, 255, 255))
-        screen.blit(tempo_troca_text, (600, 20))
+        screen.blit(tempo_troca_text, (610, 18))
+        screen.blit(self.barra_vertical_icon, (660, 0))
 
         # Desenhar o tempo total decorrido
-        screen.blit(self.tempo_texto_icon, (700, 20))
+        screen.blit(self.tempo_texto_icon, (680, 20))
         tempo_total_text = self.font.render(str(self.controlador.tempo // 1000), True, (255, 255, 255))
-        screen.blit(tempo_total_text, (900, 20))
+        screen.blit(tempo_total_text, (800, 18))
+        screen.blit(self.barra_vertical_icon, (920, 0))
 
         # Desenhar a pontuação
-        #screen.blit(self.score_texto_icon, (280, 10))
-        #score_text = self.font.render(str(self.controlador.pontuacao), True, (255, 255, 255))
-        #screen.blit(score_text, (320, 10))
+        screen.blit(self.score_texto, (950, 20))
+        score_text = self.font.render(str(self.controlador.pontuacao), True, (255, 255, 255))
+        screen.blit(score_text, (1060, 18))
 
         pygame.display.flip()
 
     def update(self):
-        pass  # Neste caso, nada precisa ser atualizado
+        pass
