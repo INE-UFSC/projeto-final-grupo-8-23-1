318a0b26068dd3da01def677bd6d4d04abe026cc
Pedro Henrique de Sena Trombini Taglialenha <phtaglialenha@gmail.com>
2023-06-21 10:35:30 -0300

    Mergeado manual o controlador v2 com o v1.5

--- versao_final/controlador.py
+++ versao_final/controlador.py
@@ -1,9 +1,8 @@
 import pygame
 import random
-import time 
+import time
 
-from entidade import Enemy, Player, Platform
-
+from entidade import Player, Platform
 from mario import Mario
 from shooter import Shooter
 from flappy import Flappy
@@ -11,6 +10,7 @@ from flappy import Flappy
 from hud import Hud
 from gameover import GameOver
 from menu import Menu
+
 
 class Controlador:
     def __init__(self, screen):
@@ -35,6 +35,9 @@ class Controlador:
         self.tempo = 0
         self.configurar()
 
+    def set_jogo(self, jogo_nome):
+        self.jogo_atual = self.jogos[jogo_nome]
+
     def configurar(self):
         self.jogos_disponiveis = list(self.jogos.values()).copy()
         self.inicio_jogo = time.time()
@@ -45,9 +48,6 @@ class Controlador:
         self.tempo_na_fase = 0
         self.inimigos = []
         self.tempo = 0
-         
-    def set_jogo(self, jogo_nome):
-        self.jogo_atual = self.jogos[jogo_nome]
 
     def contar_tempo(self):
         self.tempo = time.time() - self.inicio_jogo
@@ -76,37 +76,41 @@ class Controlador:
         for inimigo in self.inimigos:
             inimigo.rect.y -= 10
 
+    def refazer_inimigos(self):
+        self.inimigos = self.novo_jogo.get_inimigos()
+        self.player = self.novo_jogo.get_player()
+        self.plataforma = self.novo_jogo.get_plataformas()
+        self.change_enemy()
+        self.novo_jogo = self.jogo_atual(self.screen, [], self.player, self.inimigos, self.plataforma)
+
+    def game_over_reset(self):
+        self.jogos_disponiveis = list(self.jogos.values()).copy()
+        gameover_screen = GameOver(self.screen, self.pontuacao, self)
+        nome_jogo = gameover_screen.run()
+        self.running = False
+        if nome_jogo in self.jogos:
+            self.jogo_atual = self.jogos[nome_jogo]
+            self.novo_jogo = self.jogo_atual(self.screen, [], self.player, self.inimigos, self.plataforma)
+            self.running = True
+        self.inicio_jogo = time.time()
+        self.player.lives = 3
+        self.num_fases = 0
+
     def run(self):
         menu = Menu(self.screen)
         nome_jogo = menu.main()
         self.jogo_atual = self.jogos[nome_jogo]
-        jogo = self.jogo_atual(self.screen, [], self.player, self.inimigos, self.plataforma)
-        self.inicio_jogo = time.time() # começa a contar o tempo
+        self.novo_jogo = self.jogo_atual(self.screen, [], self.player, self.inimigos, self.plataforma)
+        self.inicio_jogo = time.time()
         while self.running:
             self.screen.fill((0, 0, 0))
-            jogo.run()
+            self.novo_jogo.run()
             self.hud.draw(self.screen)
             self.update()
             pygame.display.flip()
             if self.tempo_na_fase >= self.tempo_troca_de_fase:
                 self.mudar_jogo()
-
-                self.inimigos = jogo.get_inimigos()
-                self.player = jogo.get_player()
-                self.plataforma = jogo.get_plataformas()
-
-                self.change_enemy()
-
-                jogo = self.jogo_atual(self.screen, [], self.player, self.inimigos, self.plataforma)
-
+                self.refazer_inimigos()
             if self.player.lives <= 0:
-                gameover_screen = GameOver(self.screen, self.pontuacao, self)
-                nome_jogo = gameover_screen.run()
-                self.running = False
-                if nome_jogo in self.jogos:
-                    self.jogo_atual = self.jogos[nome_jogo]
-                    jogo = self.jogo_atual(self.screen, [], self.player, self.inimigos, self.plataforma)
-                    self.running = True
-                self.configurar()
-
+                self.game_over_reset()
         pygame.quit()
