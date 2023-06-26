8d47cd5c14412b5a06dc394619caec10076a45fd
Pedro Henrique de Sena Trombini Taglialenha <phtaglialenha@gmail.com>
2023-06-25 23:21:27 -0300

    Adicionado componentes animados ao menu, criado classe que cuida da animacao

--- versao_final/menu.py
+++ versao_final/menu.py
@@ -1,23 +1,39 @@
-import pygame
-from pygame.locals import *
 from highscore import Highscore
+from gifanimation import Gif
+import pygame
 
 
 class Menu:
     def __init__(self, screen):
         self.screen = screen
+        self.clock = pygame.time.Clock()  # Create a clock object
         self.background = pygame.image.load('assets/assets_menu/tela_de_fundo_menu.png')
         self.highscore_icon = pygame.image.load('assets/assets_menu/highscore.png')
         self.start_button = pygame.image.load('assets/assets_menu/botao_jogar.png')
         self.quit_button = pygame.image.load('assets/assets_menu/botao_tutorial.png')
-        self.selection_icon1 = pygame.image.load('assets/assets_menu/selecionador.png')
-        self.selection_icon2 = pygame.image.load('assets/assets_menu/selecionador2.png')
+
+        # Load the frames of the gifs into lists
+        selection_icon1_frames = [pygame.image.load(f'assets/assets_menu/selecionador_frame{i}.png') for i in range(2)]
+        selection_icon2_frames = [pygame.image.load(f'assets/assets_menu/selecionador2_frame{i}.png') for i in range(2)]
+        press_space_frames = [pygame.image.load(f'assets/assets_menu/pressionar_espaco_frame{i}.png') for i in range(13)]
+
+        # Create Gif objects
+        selection_icon1_durations = [500] * 2
+        selection_icon2_durations = [500] * 2
+        press_space_durations = [1200 if i in [0, 7] else 100 for i in range(13)]
+
+        # Create Gif objects
+        self.selection_icon1 = Gif(selection_icon1_frames, selection_icon1_durations)
+        self.selection_icon2 = Gif(selection_icon2_frames, selection_icon2_durations)
+        self.press_space = Gif(press_space_frames, press_space_durations)
+
         self.buttons = [self.start_button, self.quit_button]
         self.current_selection = 0
         self.button_positions = [(510, 225), (595, 418)]
-        self.selection_positions = [(500, 240), (582, 422)]
+        self.selection_positions = [(507, 240), (582, 422)]
         self.highscore = Highscore()
         self.font = pygame.font.SysFont('bahnschrift', 26)
+        self.press_space_center_position = (600, 600)
 
     def draw(self):
         self.screen.blit(self.background, (0, 0))
@@ -28,14 +44,24 @@ class Menu:
             self.screen.blit(button, self.button_positions[i])
             if i == self.current_selection:
                 if self.current_selection == 0:
-                    self.screen.blit(self.selection_icon1, self.selection_positions[i])
+                    self.screen.blit(self.selection_icon1.get_current_frame(), self.selection_positions[i])
                 elif self.current_selection == 1:
-                    self.screen.blit(self.selection_icon2, self.selection_positions[i])
+                    self.screen.blit(self.selection_icon2.get_current_frame(), self.selection_positions[i])
+        press_space_image = self.press_space.get_current_frame()
+        press_space_position = (self.press_space_center_position[0] - press_space_image.get_width() / 2,
+                                self.press_space_center_position[1] - press_space_image.get_height() / 2)
+        self.screen.blit(press_space_image, press_space_position)
         pygame.display.flip()
 
     def main(self):
         running = True
         while running:
+            dt = self.clock.tick()  # Seta os ticks para 60 e retorna o tempo desde o ultimo tick
+
+            self.selection_icon1.update(dt)
+            self.selection_icon2.update(dt)
+            self.press_space.update(dt)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -51,4 +77,5 @@ class Menu:
                         elif self.current_selection == 1:
                             pygame.quit()
                             return
+
             self.draw()
